4956b1f18ba08f343068f3fb909fc282430d2513
Oleg Kazakov <okazakov@hoyts.com.au>
2015-09-02 10:37:23 +1000

    Cherry picked commit 8905878a8785a71742dbce7441d445bf44bd3d24 from v7 branch

--- src/Umbraco.Core/Models/UmbracoEntity.cs
+++ src/Umbraco.Core/Models/UmbracoEntity.cs
@@ -287,14 +287,37 @@ namespace Umbraco.Core.Models
             }
         }
 
-        /// <summary>
-        /// Some entities may expose additional data that other's might not, this custom data will be available in this collection
-        /// </summary>
+        public override object DeepClone()
+        {
+            var clone = (UmbracoEntity) base.DeepClone();
+            //turn off change tracking
+            clone.DisableChangeTracking();
+            //This ensures that any value in the dictionary that is deep cloneable is cloned too
+            foreach (var key in clone.AdditionalData.Keys.ToArray())
+            {
+                var deepCloneable = clone.AdditionalData[key] as IDeepCloneable;
+                if (deepCloneable != null)
+                {
+                    clone.AdditionalData[key] = deepCloneable.DeepClone();
+                }
+            }
+            //this shouldn't really be needed since we're not tracking
+            clone.ResetDirtyProperties(false);
+            //re-enable tracking
+            clone.EnableChangeTracking();
+            return clone;
+        }
+
         public IList<UmbracoProperty> UmbracoProperties { get; set; }
-        
-        internal class UmbracoProperty : IDeepCloneable
+
+        /// <summary>
+        /// A struction that can be contained in the additional data of an UmbracoEntity representing 
+        /// a user defined property
+        /// </summary>
+        public class UmbracoProperty : IDeepCloneable
         {
             public Guid DataTypeControlId { get; set; }
+            public string PropertyEditorAlias { get; set; }
             public string Value { get; set; }
             public object DeepClone()
             {
